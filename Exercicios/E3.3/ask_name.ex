71425c7aea528008cd852ae510fa8d2a1bf8ec66
= <=>
2022-06-10 21:07:16 -0300

    feat: ask name program

--- Exercicios/E3.3/ask_name.ex
+++ Exercicios/E3.3/ask_name.ex
@@ -1,1 +1,18 @@
 # ask_name.ex
+defmodule AskName do
+  @moduledoc """
+    Provides functions to greet a people with their name
+  """
+
+  def ask(), do: IO.gets("What's your name? ") |> String.trim()
+
+  def answer("your name") do
+    IO.gets("Please, tell me your name: ") |> String.trim() |> answer()
+  end
+
+  def answer(name) when is_binary(name), do: "Hello #{name}, how are you?"
+
+  def greet(), do: ask() |> answer()
+end
+
+IO.puts(AskName.greet())
