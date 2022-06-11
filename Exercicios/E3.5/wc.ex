14779372601d16565ece1df0082dacab02decd96
= <=>
2022-06-10 23:13:01 -0300

    feat: wc program

--- Exercicios/E3.5/wc.ex
+++ Exercicios/E3.5/wc.ex
@@ -1,1 +1,32 @@
 # wc.ex
+defmodule WC do
+  @moduledoc """
+   this module implements the wc from linux
+  """
+
+  @doc """
+    this function get the filename from arg and compute the wc
+  """
+  def main do
+    System.argv() |> hd |> String.trim() |> main()
+  end
+
+  @doc """
+
+    # Params
+    - filename: the file to read the content
+
+    this function count the rows, words and chars from an file
+  """
+  def main(filename) do
+    content = File.read!(filename)
+
+    rows = content |> String.graphemes() |> Enum.count(&(&1 == "\n"))
+    words = content |> String.split(~r{\n|\s+}) |> Enum.filter(&(&1 != "")) |> Enum.count()
+    chars = content |> String.length()
+
+    IO.puts("  #{rows} #{words} #{chars} #{filename}")
+  end
+end
+
+WC.main()
